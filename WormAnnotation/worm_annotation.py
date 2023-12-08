c7bd95d3799f080d1e74ba26b9c913680a5116d1
WeheliyeHashi <88719125+WeheliyeHashi@users.noreply.github.com>
2023-12-08 09:11:29 +0000

    updated

--- WormAnnotation/worm_annotation.py
+++ WormAnnotation/worm_annotation.py
@@ -5,7 +5,7 @@ from WormAnnotation._dock_widget import Training_label
 import warnings
 warnings.filterwarnings("ignore")
 
-viewer = napari.Viewer()
+viewer = napari.Viewer(title='Behavgenom: Worm Annotator ')
 viewer.window.add_dock_widget(Training_label(viewer), area="right")
 napari.run()
 # %%
